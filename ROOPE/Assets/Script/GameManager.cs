b342cc2db2c17102526e2eef0393d3c5256258a2
Tamuel <eastern7star@gmail.com>
2016-06-22 22:10:47 +0900

    GameManager 생성, FILE IO 함수 포함

---
+++ ROOPE/Assets/Script/GameManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class GameManager : MonoBehaviour {
+	private const string FILE_PATH = Application.persistentDataPath + "/" + "map.dat";
+	private Hashtable MapObjects;
+
+	private float hitPoint = 3.0f;
+	private int score = 0;
+	private int numberOfRope = 20;
+	private int stage = 1;
+
+
+	// Make map objects
+	void Awake() {
+		MapObjects = new Hashtable ();
+		readMapFromFile ();
+	}
+
+
+	void WriteFile()
+	{
+		StreamWriter fileWriter = new StreamWriter ();
+		fileWriter = File.CreateText (FILE_PATH);
+
+		// write file
+		fileWriter.WriteLine ("Hello world");
+
+		fileWriter.Close ();
+	}
+
+
+	void readMapFromFile()
+	{
+		StreamReader fileReader = new StreamReader (FILE_PATH);
+
+		// read file
+		while(!fileReader.EndOfStream)
+		{
+			string line = fileReader.ReadLine ();
+		}
+
+		fileReader.Close ();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void nextStage() {
+		stage++;
+	}
+
+	public float getHP() {
+		return hitPoint;
+	}
+
+	public void setHP(int hitPoint) {
+		this.hitPoint = hitPoint;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void addScroe(int score) {
+		setScore (getScore () + score);
+	}
+
+	public int getNumberOfRope() {
+		return numberOfRope;
+	}
+
+	public void setNumberOfRope(int numberOfRope) {
+		this.numberOfRope = numberOfRope;
+	}
+
+	public int getStage() {
+		return stage;
+	}
+}
