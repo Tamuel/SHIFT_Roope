ab86dd26f716248f2bd83f5251e02e95eef76e3f
gaonK <jekjekjek0628@gmail.com>
2016-06-23 17:29:02 +0900

    Arrow 발사 구현, Barrier 생성, Arrow Controller 클래스 생성, 구현

--- ROOPE/Assets/Object/Obstacle.cs
+++ ROOPE/Assets/Object/Obstacle.cs
@@ -5,21 +5,18 @@ public class Obstacle : RObject, Collision, Move {
 
     public float speed;
     private Rigidbody2D rb;
-	public Vector3 range;
+
 
     public virtual void collideWithCharacter ()
 	{
 
 	}
-
-
-
+		
     public void move (float delta_x, float delta_y)
     {
         if (movable == true)
         {
 			rb = GetComponent<Rigidbody2D>();
-			transform.position = new Vector3 (Camera.main.gameObject.transform.position.x + range.x, Random.Range(-range.y, range.y), range.z);
 			rb.velocity = new Vector2(delta_x, delta_y) * speed;
         }
     }
