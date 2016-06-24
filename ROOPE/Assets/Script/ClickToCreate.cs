e7711f77adcfdbf41c530082aef7f7a90331f483
Geonhyeong Lim <ppemail@icloud.com>
2016-06-24 17:04:00 +0900

    줄 시각화 줄 짧아지는 기능 구현.

--- ROOPE/Assets/Script/ClickToCreate.cs
+++ ROOPE/Assets/Script/ClickToCreate.cs
@@ -4,10 +4,22 @@ using System.Collections;
 public class ClickToCreate : MonoBehaviour {
 
 	HingeJoint2D hd;
+	LineRenderer lr;
 
 	public GameObject clickPointObject;
+	public GameObject player;
 	private Transform clickPoint;
 	private GameObject instantiatedObj;
+
+	float a;
+	float b;
+
+
+
+	void Start() {
+		hd = GetComponent<HingeJoint2D> ();
+		lr = GetComponent<LineRenderer> ();
+	}
 
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
@@ -22,13 +34,29 @@ public class ClickToCreate : MonoBehaviour {
 			Debug.Log ("" + clickPoint.position.ToString ()); 
 			clickPoint.rotation = new Quaternion (0, 0, 0, 0);
 			instantiatedObj = (GameObject) Instantiate (clickPointObject, clickPoint.position, clickPoint.rotation);
-			hd = GetComponent<HingeJoint2D> ();
 			hd.connectedBody = instantiatedObj.GetComponent<Rigidbody2D>();
+
+			a = player.transform.position.x - clickPoint.position.x;
+			b = player.transform.position.y - clickPoint.position.y;
+
+			Debug.Log ("Anchor: " + hd.connectedAnchor.x + " " + hd.connectedAnchor.y);
+			hd.connectedAnchor = new Vector2 (a, b);
 		}
-	
+
 		if (Input.GetMouseButtonUp (0)) {
+			lr.SetPosition (0, player.transform.position);
+			lr.SetPosition (1, player.transform.position);
+			hd.connectedAnchor = new Vector2 (0f, 0f);
 			Destroy(instantiatedObj);
 		}
+
+		if (hd.connectedAnchor.magnitude >= 0.1) {
+			hd.connectedAnchor = new Vector2 (hd.connectedAnchor.x * 0.99f, hd.connectedAnchor.y * 0.99f);
+			lr.SetPosition (0, player.transform.position);
+			lr.SetPosition (1, clickPoint.position);
+		}
+			
+
 
 
 	}
