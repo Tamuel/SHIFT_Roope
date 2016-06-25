d09e4ba28eeb1bd6c15f63f6a4a0261075adcd81
Tamuel <eastern7star@gmail.com>
2016-06-25 17:13:16 +0900

    클래스 다이어그램 및 CameraController 수정

--- ROOPE/Assets/Script/CameraController.cs
+++ ROOPE/Assets/Script/CameraController.cs
@@ -2,17 +2,32 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-	private Vector3 position;
-	private Vector3 speed;
+	public Player player;
+
+	private Vector3 prevPosition;
+	private float centerOffset = 3;
 
 	// Use this for initialization
 	void Start () {
-		position = transform.position;
-		speed = new Vector3 (0.05f, 0, 0);
+		prevPosition = new Vector3(
+			player.transform.position.x + centerOffset,
+			this.transform.position.y,
+			this.transform.position.z
+		);
+	}
+
+	void FixedUpdate() {
+		if(prevPosition.x < player.transform.position.x + centerOffset)
+			prevPosition = new Vector3(
+				player.transform.position.x + centerOffset,
+				this.transform.position.y,
+				this.transform.position.z
+			);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.position = transform.position + speed;
+
+		transform.position = prevPosition;
 	}
 }
