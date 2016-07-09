a149ae384db8668bc7cdea2044eb163dc1394c4d
gaonK <jekjekjek0628@gmail.com>
2016-07-09 21:20:42 +0900

    Game Over Text

--- ROOPE/Assets/Object/Barrier.cs
+++ ROOPE/Assets/Object/Barrier.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Barrier : MonoBehaviour {
 
+    public Text gameOverText;
+
+    void Start ()
+    {
+        gameOverText.text = "";
+    }
+    
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.tag != "Player" && other.tag != "Rope" && other.tag != "ScaleChanger") {
 			Destroy (other.gameObject);
 		}
+
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            gameOverText.text = "Game Over";
+        }
 	}
 }
