527d6314c34ccb99bdbbe917565123439097e7e8
gaonK <jekjekjek0628@gmail.com>
2016-07-10 19:15:18 +0900

    GameOver시 Button 출현

--- ROOPE/Assets/Object/Barrier.cs
+++ ROOPE/Assets/Object/Barrier.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 public class Barrier : MonoBehaviour {
 
     public Text gameOverText;
+    public Button restartButton;
+    public Button mainButton;
 
     void Start ()
     {
         gameOverText.text = "";
+        restartButton.gameObject.SetActive(false);
+        mainButton.gameObject.SetActive(false);
     }
     
 	void OnTriggerEnter2D (Collider2D other)
@@ -21,6 +25,8 @@ public class Barrier : MonoBehaviour {
         {
             other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             gameOverText.text = "Game Over";
+            restartButton.gameObject.SetActive(true);
+            mainButton.gameObject.SetActive(true);
         }
 	}
 }
