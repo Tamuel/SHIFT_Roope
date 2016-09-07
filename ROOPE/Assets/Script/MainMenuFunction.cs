66e74c60bc364fa4185359afcaeaa7cb05a2a1e6
Tamuel <eastern7star@gmail.com>
2016-09-08 06:40:14 +0900

    Main Menu UI 수정 중, Exit 버튼 소스 수정, Play Service로 인해 Build 되지 않던 문제 수정

--- ROOPE/Assets/Script/MainMenuFunction.cs
+++ ROOPE/Assets/Script/MainMenuFunction.cs
@@ -2,18 +2,20 @@
 using System.Collections;
 
 public class MainMenuFunction : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	private bool up;
+	private float range = 0.3f;
+	private float speed;
+	private Vector3 startPosition;
 
 	public void onStartButtonClick() {
 		Application.LoadLevel("Main");
+	}
+
+	public void onScoreButtonClick() {
+		
+	}
+
+	public void onExitButtonClick() {
+		Application.Quit ();
 	}
 }
