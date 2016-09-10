978d398b1c46ce6cdbe71bce912bcf7c139ba070
Tamuel <eastern7star@gmail.com>
2016-09-10 22:24:45 +0900

    메인 메뉴 UI 작업중

--- ROOPE/Assets/Script/MainMenuFunction.cs
+++ ROOPE/Assets/Script/MainMenuFunction.cs
@@ -6,9 +6,15 @@ public class MainMenuFunction : MonoBehaviour {
 	private float range = 0.3f;
 	private float speed;
 	private Vector3 startPosition;
+	private ParticleSystem particle;
+
+	void Update() {
+		if(particle && !particle.IsAlive() == false)
+			Application.LoadLevel("Main");
+	}
 
 	public void onStartButtonClick() {
-		Application.LoadLevel("Main");
+		particle = Instantiate (Resources.Load ("Prefabs/ButtonPressParticle"), transform.position, new Quaternion()) as ParticleSystem;
 	}
 
 	public void onScoreButtonClick() {
